4747511c49f912ecb2c12a237d0affe81afd9f12
tony <tonyler@pm.me>
2023-02-26 23:08:36 +0200

    wowwwaaa

---
+++ script.py
@@ -0,0 +1,13 @@
+# Import any necessary modules
+import time
+
+# Define the script's logic
+def run():
+    time.sleep(5)  # Simulate a long-running process
+    return "Script executed successfully!"
+
+# Call the run function and return the response
+print(run())
+
+
+
