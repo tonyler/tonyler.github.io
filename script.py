35e8550bf9fbe1b453baf0208ce7b2609c7e758e
tony <tonyler@pm.me>
2023-02-26 23:26:01 +0200

    dd

--- script.py
+++ script.py
@@ -1,12 +1,2 @@
-# Import any necessary modules
-import time
-
-# Define the script's logic
-def run():
-    return "Script executed successfully!"
-
-# Call the run function and return the response
-print(run())
-
-
+print("Hello, world!")
 
