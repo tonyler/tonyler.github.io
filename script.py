f0223a8b45229fb9a3b9d7e82c7ecb62649b2ed1
tony <tonyler@pm.me>
2023-02-26 23:13:03 +0200

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,6 @@ import time
 
 # Define the script's logic
 def run():
-    time.sleep(5)  # Simulate a long-running process
     return "Script executed successfully!"
 
 # Call the run function and return the response
